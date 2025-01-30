c47934220966f65b0885195d37d922cea11b410f
MARIA <mariben2222@gmail.com>
2025-01-30 18:32:08 +0100

    Update stackk.c

--- stack.c
+++ stack.c
@@ -1,50 +1,40 @@
 #include <stdio.h>
-#include "stack.h"
-
-
-void initStack(Stack* stack) {
-    stack->top = -1;
+#include <stdlib.h>
+#include <ctype.h>  
+#include <string.h>
+#include "stack&.h"  
+#define MAX_SIZE 100  
+int precedence(char op) {
+    if (op == '+' || op == '-') return 1;
+    if (op == '*' || op == '/') return 2;
+    return 0;
 }
-
-void push(Stack* stack, int item) {
-    if (stack->top < MAX_SIZE_S - 1) {
-        stack->items[++stack->top] = item;
-    } else {
-        printf("Stack Overflow: Unable to push %d\n", item);
-    }
-}
-
-
-int pop(Stack* stack) {
-    if (stack->top >= 0) {
-        return stack->items[stack->top--];
-    } else {
-        printf("Stack Underflow: Unable to pop\n");
-        return -1; 
-    }
-}
-
-
-int peek_s(const Stack* stack) {
-    if (stack->top >= 0) {
-        return stack->items[stack->top];
-    } else {
-        printf("Stack is empty: No item to peek\n");
-        return -1;
-    }
-}
-int main() {
+void infix_to_postfix(const char* infix) {
     Stack stack;
     initStack(&stack);
-
-    push(&stack, 10);
-    push(&stack, 6);
-    push(&stack, 3);
-
-    printf("Top element is %d\n", peek_s(&stack));
-
-    printf("Popped element is %d\n", pop(&stack));
-    printf("Top element after pop is %d\n", peek_s(&stack));
-
-    return 0;
+    char postfix[MAX_SIZE] = "";
+    int k = 0;
+    for (int i = 0; infix[i] != '\0'; i++) {
+        char ch = infix[i];
+        if (isalnum(ch)) {
+            postfix[k++] = ch;
+        } 
+        else if (ch == '(') {
+            push(&stack, ch);
+        } 
+        else if (ch == ')') {
+            while (!is_empty_s(&stack) && peek_s(&stack) != '(')
+                postfix[k++] = pop(&stack);
+            pop(&stack);
+        } 
+        else {
+            while (!is_empty_s(&stack) && precedence(peek_s(&stack)) >= precedence(ch))
+                postfix[k++] = pop(&stack);
+            push(&stack, ch);
+        }
+    }
+    while (!is_empty_s(&stack))
+        postfix[k++] = pop(&stack);
+    postfix[k] = '\0'; 
+    printf("Postfix: %s\n", postfix);
 }
